06dcd9d11360ec58ba3b8fcbb4e578e674bdeb9f
Pablo-Ortiz1031 <juanpabloortiztrujillo@jesusamigo.com.co>
2025-06-19 17:51:10 -0500

    Avances jquery

--- sistemparking/sistemparking/settings.py
+++ sistemparking/sistemparking/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_filters',
     'rest_framework',
+    'corsheaders',
 
     'apps.facturacion',
     'apps.horaparqueo',
@@ -56,6 +57,7 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware"
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -64,7 +66,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'sistemparking.urls'
 
 TEMPLATES = [
