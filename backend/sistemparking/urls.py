22d7a69878b436b06c3448e128dac25d56f50049
monenco2 <keinersmithherreragalvis@jesusamigo.com.co>
2025-07-30 14:28:01 -0500

    template 2

--- backend/sistemparking/urls.py
+++ backend/sistemparking/urls.py
@@ -14,4 +14,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.routers')),
     path('usua/',auth_views.LoginView.as_view(template_name='registration/usua.html')),
+    path('factu/',auth_views.LoginView.as_view(template_name='registration/factu.html')),
+    path('hpar/',auth_views.LoginView.as_view(template_name='registration/hpar.html')),
+    path('pg/',auth_views.LoginView.as_view(template_name='registration/pg.html')),
+    path('rl/',auth_views.LoginView.as_view(template_name='registration/rl.html')),
+    path('servi/',auth_views.LoginView.as_view(template_name='registration/servi.html')),
+    path('vehi/',auth_views.LoginView.as_view(template_name='registration/vehi.html')),
 ]
