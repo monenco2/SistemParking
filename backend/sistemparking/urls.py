e24d7ab714c4d9d7b741a73311aa42a41be2a406
monenco2 <keinersmithherreragalvis@jesusamigo.com.co>
2025-07-31 14:51:13 -0500

    Template 3

--- backend/sistemparking/urls.py
+++ backend/sistemparking/urls.py
@@ -13,11 +13,11 @@ from apps.vehiculo import*
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.routers')),
-    path('usua/',auth_views.LoginView.as_view(template_name='registration/usua.html')),
-    path('factu/',auth_views.LoginView.as_view(template_name='registration/factu.html')),
-    path('hpar/',auth_views.LoginView.as_view(template_name='registration/hpar.html')),
-    path('pg/',auth_views.LoginView.as_view(template_name='registration/pg.html')),
-    path('rl/',auth_views.LoginView.as_view(template_name='registration/rl.html')),
-    path('servi/',auth_views.LoginView.as_view(template_name='registration/servi.html')),
-    path('vehi/',auth_views.LoginView.as_view(template_name='registration/vehi.html')),
+    path('usua/',auth_views.LoginView.as_view(template_name='usuarios/usua.html')),
+    path('factu/',auth_views.LoginView.as_view(template_name='facturacion/factu.html')),
+    path('hpar/',auth_views.LoginView.as_view(template_name='horaparqueo/hpar.html')),
+    path('pg/',auth_views.LoginView.as_view(template_name='pagofinal/pg.html')),
+    path('rl/',auth_views.LoginView.as_view(template_name='rol/rl.html')),
+    path('servi/',auth_views.LoginView.as_view(template_name='servicio/servi.html')),
+    path('vehi/',auth_views.LoginView.as_view(template_name='vehiculo/vehi.html')),
 ]
